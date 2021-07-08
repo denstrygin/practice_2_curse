69fb53ea8feedb76c57a61b726befed4606b15ba
Aleksandr201-1 <70757686+Aleksandr201-1@users.noreply.github.com>
2021-07-08 23:35:09 +0300

    Add files via upload

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,7 +2,7 @@
 
 #запускаем в эмуляторе qemu
 
-# ./make_initrd test.txt test.txt test2.txt test2.txt
+#./make_initrd test.txt test.txt test2.txt test2.txt
 ./make_initrd
 sudo make update_image
 sudo qemu-system-i386 -initrd initrd.img -kernel src/MiniOS.bin
