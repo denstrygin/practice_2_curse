ab1bb74668a2ffa8b84284fc6b97e783f7df6577
Aleksandr201-1 <70757686+Aleksandr201-1@users.noreply.github.com>
2021-07-11 00:09:40 +0300

    Add files via upload

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,7 +2,6 @@
 
 #запускаем в эмуляторе qemu
 
-#./make_initrd test.txt test.txt test2.txt test2.txt
-./make_initrd
-sudo make update_image
-sudo qemu-system-i386 -initrd initrd.img -kernel src/MiniOS.bin
+./make_initrd test.txt test.txt test2.txt test2.txt #форматируем диск и добавляем файлы test.txt и text2.txt
+sudo make update_image #компилим исходники в папке src в ядро и обновляем ядро в нашем образе
+sudo qemu-system-i386 -initrd initrd.img -kernel src/MiniOS.bin #запускаем qemu
